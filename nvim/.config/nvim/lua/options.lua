187bc1807b088d5730b3ff3edfcf36673ddd31a9
Lars Lindberg <absolute.dessan@proton.me>
2025-08-06 00:08:09 +0200

    nvim mappings/options.lua

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,18 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.filetype.add({
+  filename = {
+    ["nginx.conf"] = "nginx",
+  },
+  pattern = {
+    [".*/nginx/.*%.conf$"] = "nginx",
+    [".*/nginx/conf%.d/.*%.conf$"] = "nginx",
+    [".*/sites%-available/.*$"] = "nginx",
+    [".*/sites%-enabled/.*$"] = "nginx",
+  },
+})
