de483cc881198a0c7c68d477438286fb7a5edc8d
Lars Lindberg <absolute.dessan@proton.me>
2025-12-14 16:54:01 +0100

    VimEnter/Exit

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,1 +1,11 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("VimEnter", {
+  command = ":silent !kitty @ set-spacing padding=0 margin=0",
+})
+
+autocmd("VimLeavePre", {
+  command = ":silent !kitty @ set-spacing padding=8 margin=0",
+})
