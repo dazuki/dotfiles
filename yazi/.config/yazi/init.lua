329a09be3c60152ab3eb16f466518cb3a3bd0c36
Lars Lindberg <absolute.dessan@proton.me>
2026-01-01 13:57:23 +0100

    yazi plugin changes and edits

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,1 +1,8 @@
-require("no-status"):setup()
+require("git"):setup()
+
+require("omp"):setup({ config = "/home/dazuki/.config/ohmyposh/dazuki.toml" })
+
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.PLAIN,
+})
